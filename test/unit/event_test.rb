32111f6abcf929676087c558a68137223d2f7e87
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-02-25 00:23:18 -0800

    had to get rid of old tests from deleted models, practiced running tests but it was SUPER FUCKING BORING

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
+
   # Replace this with your real tests.
   test "the truth" do
     assert true
   end
+  
+  test "should not save event without name" do
+    event = Event.new
+    assert !event.save
+  end
+
 end
